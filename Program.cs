83850e8b16e77dcadf204522a81081f1744d0e2b
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-03-24 08:39:47 +0530

    Logical questions...

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//2.WAP for getting a square root of a given number.
+using System;
+namespace logicals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to get the SquareRoot : ");
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"SquareRoot of {n} is " + Math.Sqrt(n));
+        }
+    }
+}
